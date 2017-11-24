d63a6b1137f08ff7ba20c0b95518349eb76ec2af
petercz1 <email.peter.carroll@gmail.com>
2017-11-24 14:40:56 +0800

    Man who sleep in beer keg wake up sticky.

--- public/js/frontend_app.js
+++ public/js/frontend_app.js
@@ -1,6 +1,6 @@
 console.log('loaded frontend app');
 
-var frontend_app = angular.module('presidents_app', ['ui.bootstrap','uib/template/alert/alert.html'])
+var frontend_app = angular.module('presidents_app', ['ui.bootstrap','/template/alert/alert.html'])
 frontend_app.controller('presidents_data', do_presidents);
 
 function do_presidents($scope, $http) {
